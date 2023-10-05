6d34468070d5a7af54a0ec7193e6016b10f8337c
EmitoStuer <stuer.87@gmail.com>
2023-10-05 15:19:03 -0300

    Vista login

--- Proyecto-Restaurante/src/proyecto/restaurante/Vistas/LoginMesero.java
+++ Proyecto-Restaurante/src/proyecto/restaurante/Vistas/LoginMesero.java
@@ -5,6 +5,8 @@
  */
 package proyecto.restaurante.Vistas;
 
+import java.awt.Color;
+
 /**
  *
  * @author Emito
@@ -29,7 +31,7 @@ public class LoginMesero extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel7 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
+        background = new javax.swing.JPanel();
         LogoMesero = new javax.swing.JLabel();
         loginMesero = new javax.swing.JLabel();
         Password = new javax.swing.JLabel();
@@ -37,65 +39,86 @@ public class LoginMesero extends javax.swing.JFrame {
         jtUsuario = new javax.swing.JTextField();
         jbRegistrarse = new javax.swing.JButton();
         jbIngresar = new javax.swing.JButton();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        jtPassword = new javax.swing.JPasswordField();
         Fondo = new javax.swing.JLabel();
 
         jLabel7.setText("jLabel7");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
+        setLocationByPlatform(true);
         setResizable(false);
 
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        background.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         LogoMesero.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         LogoMesero.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyecto/restaurante/resources/imagenes/mesero.png"))); // NOI18N
-        jPanel1.add(LogoMesero, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 150, 200, 200));
+        background.add(LogoMesero, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 150, 200, 200));
 
-        loginMesero.setFont(new java.awt.Font("Comic Sans MS", 1, 20)); // NOI18N
+        loginMesero.setFont(new java.awt.Font("Comic Sans MS", 3, 20)); // NOI18N
         loginMesero.setForeground(new java.awt.Color(255, 255, 255));
         loginMesero.setText("Login Meseros");
-        jPanel1.add(loginMesero, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 100, -1, -1));
+        background.add(loginMesero, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 100, -1, -1));
 
         Password.setFont(new java.awt.Font("Comic Sans MS", 1, 17)); // NOI18N
         Password.setForeground(new java.awt.Color(255, 255, 255));
         Password.setText("Password:");
-        jPanel1.add(Password, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 420, -1, -1));
+        background.add(Password, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 420, -1, -1));
 
         Usuario.setFont(new java.awt.Font("Comic Sans MS", 1, 17)); // NOI18N
         Usuario.setForeground(new java.awt.Color(255, 255, 255));
         Usuario.setText("Usuario:");
-        jPanel1.add(Usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 370, -1, -1));
+        background.add(Usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 370, -1, -1));
 
+        jtUsuario.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jtUsuario.setForeground(new java.awt.Color(204, 204, 204));
+        jtUsuario.setText("Ingrese Dni");
+        jtUsuario.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jtUsuarioMousePressed(evt);
+            }
+        });
         jtUsuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jtUsuarioActionPerformed(evt);
             }
         });
-        jPanel1.add(jtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 370, 220, -1));
+        background.add(jtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 370, 220, -1));
 
+        jbRegistrarse.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
         jbRegistrarse.setText("Registrarse");
-        jPanel1.add(jbRegistrarse, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 490, 140, -1));
+        background.add(jbRegistrarse, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 490, 140, -1));
 
+        jbIngresar.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
         jbIngresar.setText("Ingresar");
-        jPanel1.add(jbIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 490, 150, -1));
-        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 420, 220, -1));
+        background.add(jbIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 490, 150, -1));
+
+        jtPassword.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jtPassword.setForeground(new java.awt.Color(204, 204, 204));
+        jtPassword.setText("********");
+        jtPassword.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jtPasswordMousePressed(evt);
+            }
+        });
+        background.add(jtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 420, 220, -1));
 
         Fondo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyecto/restaurante/resources/imagenes/fondoLogin.jpeg"))); // NOI18N
         Fondo.setMaximumSize(new java.awt.Dimension(400, 500));
         Fondo.setMinimumSize(new java.awt.Dimension(400, 500));
-        jPanel1.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 600));
+        background.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 600));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -104,6 +127,28 @@ public class LoginMesero extends javax.swing.JFrame {
     private void jtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtUsuarioActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jtUsuarioActionPerformed
+
+    private void jtUsuarioMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtUsuarioMousePressed
+        if (jtUsuario.getText().equalsIgnoreCase("Ingrese Dni")){
+        jtUsuario.setText("");
+        jtUsuario.setForeground(Color.black);
+        }
+        if(String.valueOf(jtPassword.getPassword()).isEmpty()){
+        jtPassword.setText("********");
+        jtPassword.setForeground(Color.gray);
+        }
+    }//GEN-LAST:event_jtUsuarioMousePressed
+
+    private void jtPasswordMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtPasswordMousePressed
+        if (String.valueOf(jtPassword.getPassword()).equals("********")){
+        jtPassword.setText("");
+        jtPassword.setForeground(Color.black);
+        }
+        if (jtUsuario.getText().isEmpty()){
+        jtUsuario.setText("Ingrese Dni");
+        jtUsuario.setForeground(Color.gray);
+        }
+    }//GEN-LAST:event_jtPasswordMousePressed
 
     /**
      * @param args the command line arguments
@@ -145,11 +190,11 @@ public class LoginMesero extends javax.swing.JFrame {
     private javax.swing.JLabel LogoMesero;
     private javax.swing.JLabel Password;
     private javax.swing.JLabel Usuario;
+    private javax.swing.JPanel background;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JButton jbIngresar;
     private javax.swing.JButton jbRegistrarse;
+    private javax.swing.JPasswordField jtPassword;
     private javax.swing.JTextField jtUsuario;
     private javax.swing.JLabel loginMesero;
     // End of variables declaration//GEN-END:variables
