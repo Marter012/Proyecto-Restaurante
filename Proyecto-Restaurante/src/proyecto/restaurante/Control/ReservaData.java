e86adbe6faf42ff1c9abdde9cf0be5a9d21fd732
FerMSz <fermsz.89@gmail.com>
2023-10-08 13:32:31 -0300

    ReservaData

--- Proyecto-Restaurante/src/proyecto/restaurante/Control/ReservaData.java
+++ Proyecto-Restaurante/src/proyecto/restaurante/Control/ReservaData.java
@@ -8,6 +8,8 @@ package proyecto.restaurante.Control;
 import java.sql.*;
 import java.sql.Date;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import proyecto.restaurante.Entidades.Mesa;
 import proyecto.restaurante.Entidades.Reserva;
@@ -56,11 +58,38 @@ public ReservaData(){
         
  }
 
- 
-    public List<Reserva> listarReservas(){
-        List<Reserva> listaReservas= new Arraylist();
+    public void buscarReserva(int DNI){
        
-        sql="SELECT * FROM reserva";
+        sql="SELECT * FROM reservas WHERE DNI=?";
+      
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setInt(1, DNI);
+            rs=ps.executeQuery();
+            
+             if(rs.next()){
+                 res=new Reserva();
+                 res.setIdReserva(rs.getInt("idReserva"));
+                 res.setNombreCliente(rs.getString("NombreCliente"));
+                 res.setDni(rs.getInt("DNI"));
+                 res.setFecha(rs.getDate("Fecha").toLocalDate());
+                 res.setHora(rs.getTime("Hora").toLocalTime());
+                 res.setEstado(rs.getBoolean("Estado"));
+                 res.setMesa(md.obtenerMesa(rs.getInt("idMesa")));
+             }else{
+                JOptionPane.showMessageDialog(null, "ReservaData : No se encontró reserva con DNI: "+DNI);      
+            }
+            ps.close();
+      } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ReservaData: Error al buscar reserva "+ex.getMessage());
+      }
+        return res;
+    }
+ 
+    public ArrayList<Reserva> listarReservas(){
+        ArrayList <Reserva> listaReservas = new ArrayList();
+       
+        sql="SELECT * FROM reservas";
         
         try {
             ps=con.prepareStatement(sql);
@@ -84,8 +113,77 @@ public ReservaData(){
         return listaReservas;
     }
 
+public ArrayList<Reserva> listarReservasPorFecha(Date Fecha){
+        ArrayList <Reserva> reservasPorFecha = new ArrayList();
+       
+        sql="SELECT * FROM reserva WHERE fecha=?";
+        
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setDate(1,Fecha);
+            rs=ps.executeQuery();
+          
+            while (rs.next()){
+               res=new Reserva();
+               res.setNombreCliente(rs.getString(1));
+               res.setDni(rs.getInt(2));
+               res.setFecha(rs.getDate(3).toLocalDate());
+               res.setHora(rs.getTime(4).toLocalTime());
+               res.setEstado(rs.getBoolean(5));
+               res.setMesa(md.obtenerMesa(rs.getInt(6)));
+               reservasPorFecha.add(res);
+            }
+            ps.close();
+             
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"ReservaData: Error al obtener lista de reservas en la fecha seleccionada"+ex.getMessage());
+        }
+        return reservasPorFecha;
+    }
+     
+    public void modificarReserva(Reserva res){
+        sql="UPDATE reservas SET NombreClientE=?,DNI=?,Fecha=?,Hora=?,Estado=?,idMesa=? WHERE idReserva=?";
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setString(1,res.getNombreCliente());
+            ps.setInt(2, res.getDni());
+            ps.setDate(3,Date.valueOf(res.getFecha()));
+            ps.setTime(4,Time.valueOf(res.getHora()));
+            ps.setBoolean(5,res.isEstado());
+            ps.setInt(6,res.getMesa().getIdMesa());
+            ps.setInt(7,res.getIdReserva());
+            int resultSet= ps.executeUpdate();
+            
+              if(resultSet==1){
+                    JOptionPane.showMessageDialog(null,"ReservaData : Reserva modificada correctamente");
+              }else{
+                JOptionPane.showMessageDialog(null,"ReservaData : No se encontró reserva para modificar");      
+              }            
+              ps.close();
+              
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ReservaData : Error al modificar la Reserva"+ex.getMessage()); 
+        }
+    }
 
-    
+    public void eliminarReserva(int DNI){ 
+        sql= "UPDATE reservas SET estado=? WHERE DNI=?";
+                
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setBoolean(1, false);
+            ps.setInt(2,DNI);
+            int resultSet= ps.executeUpdate();
+            if(resultSet==1){
+                JOptionPane.showMessageDialog(null, "ReservaData : Reserva eliminada con exito.");
+            }else{
+                JOptionPane.showMessageDialog(null, "ReservaData : No se encontró la reserva");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"ReservaData: Error al eliminar la reserva"+ex.getMessage());
+        }
+        
+    }
 
    
 }
