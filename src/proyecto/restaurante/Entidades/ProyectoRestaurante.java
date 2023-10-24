455edde8a38b850f3bac2177e72811f11d1b9ac5
alexM4xi <alexesquivel136@gmail.com>
2023-10-24 10:53:36 +0100

    cambiios

---
+++ src/proyecto/restaurante/Entidades/ProyectoRestaurante.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.restaurante.Entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+import proyecto.restaurante.Control.*;
+
+/**
+ *
+ * @author Emito
+ */
+public class ProyectoRestaurante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Producto p = new Producto("Pizza", 10, Categoria.ALIMENTO, 900.50,true);
+//        ProductoData pd = new ProductoData();
+//        Producto p = new Producto("Coca Cola", 20, Categoria.BEBIDA, 700,true);
+//        pd.guardarProducto(p);
+//        System.out.println(pd.listarProductos());
+
+    
+    
+    
+    
+    PedidoData pd = new PedidoData();
+    MesaData md = new MesaData();
+    
+    MeseroData med = new MeseroData();
+    System.out.println(med.buscarMeseroPorDNI(41521048));
+    
+//    PedidoData pd = new PedidoData();
+//    MesaData md = new MesaData();
+    }
+    
+    
+}
